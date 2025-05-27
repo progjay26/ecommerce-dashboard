01bbd68835b99c77c1f83d078cef0df819148d4b
progjay26 <frempongkwame26@gmail.com>
2025-05-27 02:10:55 +0000

    feat: Add Tailwind CSS v3 configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all JS, JSX, TS, TSX files in the src folder
+  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
